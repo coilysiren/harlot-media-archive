bcaa70c763234756c5c5135a1b97a046f670d41d
Lynn Cyrin <firemagelynn@gmail.com>
2016-02-22 22:28:18 -0800

    copy user photes from production

--- bin/copy-remote-uploads.bash
+++ bin/copy-remote-uploads.bash
@@ -1,2 +1,4 @@
 mkdir public_html/uploads
 scp -r root@harlot.media:/var/www/vhosts/harlot.media/httpdocs/public_html/uploads/* public_html/uploads
+mkdir craft/storage/userphotos
+scp -r root@harlot.media:/var/www/vhosts/harlot.media/httpdocs/craft/storage/userphotos/* craft/storage/userphotos
