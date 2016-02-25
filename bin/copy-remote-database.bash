e1e5d85b7e9db09e809fd9a7f2e2c4a75a8005c9
Lynn Cyrin <firemagelynn@gmail.com>
2016-02-25 15:13:39 -0800

    fix database copy script

--- bin/copy-remote-database.bash
+++ bin/copy-remote-database.bash
@@ -1,4 +1,4 @@
-ssh root@harlot.media -t "mysqldump --user="harlot_dbuser" -p" -A > harlot_media_dump.sql ;"
+ssh root@harlot.media -t "mysqldump --user='harlot_dbuser' -p -A > harlot_media_dump.sql ;"
 scp root@harlot.media:harlot_media_dump.sql /tmp/harlot_media_dump.sql
 ssh root@harlot.media -t "rm -f harlot_media_dump.sql ;"
-mysql --user="root" --password="lynn" < /tmp/harlot_media_dump.sql
+mysql --user="root" -p < /tmp/harlot_media_dump.sql
