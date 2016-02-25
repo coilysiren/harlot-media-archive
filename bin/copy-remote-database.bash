5b12be266b5acfa6fb262e28d73465f844ab3d6e
Lynn Cyrin <firemagelynn@gmail.com>
2016-02-25 15:20:47 -0800

    some directions

--- bin/copy-remote-database.bash
+++ bin/copy-remote-database.bash
@@ -1,4 +1,6 @@
+echo "input harlot_dbuser password for database at harlot.media"
 ssh root@harlot.media -t "mysqldump --user='harlot_dbuser' -p -A > harlot_media_dump.sql ;"
 scp root@harlot.media:harlot_media_dump.sql /tmp/harlot_media_dump.sql
 ssh root@harlot.media -t "rm -f harlot_media_dump.sql ;"
+echo "input local database password"
 mysql --user="root" -p < /tmp/harlot_media_dump.sql
