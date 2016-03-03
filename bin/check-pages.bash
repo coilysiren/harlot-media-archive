8a6aa9f5e559387c108a3ab6a04c6823bf3b06aa
Lynn Cyrin <firemagelynn@gmail.com>
2016-03-02 19:12:02 -0800

    add page checking script

---
+++ bin/check-pages.bash
@@ -0,0 +1,16 @@
+# use:
+#   bash bin/check-pages localhost
+#   bash bin/check-pages harlot.media
+
+xdg-open http://$1/ &&
+xdg-open http://$1/about &&
+xdg-open http://$1/donate &&
+xdg-open http://$1/contact &&
+xdg-open http://$1/comment-policy &&
+xdg-open http://$1/content-warnings &&
+xdg-open http://$1/donate_thank_you &&
+xdg-open http://$1/search &&
+xdg-open http://$1/rss &&
+xdg-open http://$1/authors/cyrin &&
+xdg-open http://$1/articles/more &&
+xdg-open http://$1/articles/690/samus-is-trans-and-ronda-rousey-probably-shouldnt-play-her
