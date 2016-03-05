ad2fa4732c147ca7ed52b5f293e2018720681657
Lynn Cyrin <firemagelynn@gmail.com>
2016-03-05 10:05:54 -0800

    Title Length and Article Preview changes

--- public_html/_assets/js/main.js
+++ public_html/_assets/js/main.js
@@ -8,5 +8,21 @@ function resize_article_borders() {
     })
 }
 
+function hide_ellipses() {
+    $(".article-preview-container").each(function(){
+        content_bottom = $(this).find(".border-bottom")[0].getBoundingClientRect()["bottom"]
+        description_bottom = $(this).find(".description-end")[0].getBoundingClientRect()["bottom"]
+        console.log($(this).find(".description").text())
+        console.log(content_bottom)
+        console.log(description_bottom)
+        if (description_bottom <= (content_bottom+1))
+            $(this).find(".overflow-fade").css("display", "none")
+            console.log('overflowing')
+    })
+}
+
 resize_article_borders()
 $(window).resize(resize_article_borders)
+
+hide_ellipses()
+$(window).resize(hide_ellipses)
