97b40311772accd456793aa2223eb9db20762dc9
Lynn Cyrin <firemagelynn@gmail.com>
2016-03-25 12:24:07 -0700

    Formatting change

--- bin/deploy-changes.bash
+++ bin/deploy-changes.bash
@@ -1,2 +1,6 @@
 git push
-ssh root@harlot.media -t "cd /var/www/vhosts/harlot.media/httpdocs/ ; git pull ; sass --update public_html/_assets/sass:public_html/_assets/css -t compressed"
+ssh root@harlot.media -t "
+    cd /var/www/vhosts/harlot.media/httpdocs/ ;
+    git pull ;
+    sass --update public_html/_assets/sass:public_html/_assets/css -t compressed ;
+    "
